35d684adabf526c3c8f4307ecc9f7dcc62347d0a
FrankHu <frank.hu@perficient.com>
2020-08-18 14:00:19 +0800

    dsfsd

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const fs = require("fs");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  console.log(req);
+  fs.writeFileSync("../server/public/1.html", "Hello world");
+  // res.render("index", { title: "Express" });
 });
 
 module.exports = router;
